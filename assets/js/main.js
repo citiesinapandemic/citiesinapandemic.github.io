fc06b5b21c0fb722491272f3e3fef25eafd73d18
ritchykemp <ritchy.kemp@gmail.com>
2021-02-05 10:15:28 +0100

    updating  Countdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,7 +65,7 @@
 
 })(jQuery);
 
-var countDownDate = new Date("Feb 7, 2021 15:37:25").getTime();
+var countDownDate = new Date("Feb 7, 2021 22:37:25").getTime(); // +7h für Bangkok
 
 // Update the count down every 1 second
 var x = setInterval(function() {
