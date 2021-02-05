f0cc8597e03786e9495dca1a54f7bf990ac2a9e2
ritchykemp <ritchy.kemp@gmail.com>
2021-02-05 09:36:36 +0100

    selection option for cities + coutdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,8 +65,34 @@
 
 })(jQuery);
 
-var foo = $('#nav > ul');
-foo.dropotron();
+var countDownDate = new Date("Feb 7, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("countdown").innerHTML ="The survey starts in: <b>"+ days + "</b> day <b>" + hours + "</b> hours <b>" + minutes + "</b> minutes <b>" + seconds + "</b> seconds ";
+	
+  console.log(document.getElementById("countdown").innerHTML)
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "You Can start the survey right now!";
+  }
+}, 1000);
+
 
 	window.cookieconsent.initialise({
 		"palette": {
